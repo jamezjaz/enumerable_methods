0673a7e6fc45abd23060c4ff8ecc149c02b95376
Jamezjaz <jamezjaz@gmail.com>
2020-05-02 07:20:56 -0500

    add my_map_with_proc_or_block

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -69,7 +69,16 @@ module Enumerable
       new_arr
     end
 
+    def my_map_with_proc_or_block
+      new_arr = []
+      for item in self
+        new_arr << proc.call(item) || yield(item)
+      end  
+      new_arr
+    end
 end
 
-# arr = [1, 3, 4, 4, 10, 9, 21, 30]
-# p arr.my_map_with_proc {|num| num < 5}
+# Test cases for my_map_with_proc_or_block
+# arr = [2, 3, 6, 80, 27, 10]
+# p arr.my_map_with_proc_or_block { |sum, item| sum % 3 == 0 }
+# Should return [true, true, true, true, false, false, false, false]
