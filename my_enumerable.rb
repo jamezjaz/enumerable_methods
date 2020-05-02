2334e69aa9ae626f8f854b89475556d0ae5d5979
Jamezjaz <jamezjaz@gmail.com>
2020-05-02 06:28:19 -0500

    add my_map_with_proc

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -60,7 +60,16 @@ module Enumerable
         end  
         new_arr
     end 
+
+    def my_map_with_proc
+      new_arr = []
+      for item in self do 
+        new_arr << proc.call(item)
+      end  
+      new_arr
+    end
+
 end
 
-arr = [1, 3, 4, 4, 10, 9, 21, 30]
-p arr.my_map {|num| num < 5}
+# arr = [1, 3, 4, 4, 10, 9, 21, 30]
+# p arr.my_map_with_proc {|num| num < 5}
