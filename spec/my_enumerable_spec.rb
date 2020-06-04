30da89876b1986df2ec374a3c968d97e009e59d8
Jamezjaz <jamezjaz@gmail.com>
2020-06-04 11:29:13 -0500

    Adds test for #my_select method

--- spec/my_enumerable_spec.rb
+++ spec/my_enumerable_spec.rb
@@ -2,11 +2,12 @@ require './my_enumerable'
 
 describe Enumerable do
     let(:array1) { [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }
-    let(:array2) { %w[hello hey hi holla yea good] }
+    let(:array2) { %w[microverse mark men meat ruby coding] }
     let(:array3) { [1, 2, 3, 4] }
     let(:array4) { [2, 4, 6, 8] }
     let(:array5) { [2, 4, 6, 8, 'jersey', %w[why me cr7 oa7]] }
     let(:array6) { [4, 2, 3, 4, 4, 3, 5, 4, 4] }
+    let(:array7) { [1, 3, 5, 7, 9] }
     let(:new_array) { [] }
 
     describe '#my_each' do
@@ -31,6 +32,24 @@ describe Enumerable do
 
         it 'returns the Enumerator when no block is given' do
             expect(array1.my_each).to be_kind_of(Enumerator)
+        end
+    end
+
+    describe '#my_select' do
+        it 'returns new array based on argument passed' do
+            array1.my_select { |item| new_array << item if item > 0 and item % 2 == 1 }
+
+            expect(new_array).to eql(array7)
+        end
+
+        it 'returns a specific data based on argument passed' do
+            array2.my_select { |item| new_array << item if item.include? 'm' }
+
+            expect(new_array).to eql(%w[microverse mark men meat])
+        end
+
+        it 'returns the Enumerator when no block is given' do
+            expect(array1.my_select).to be_kind_of(Enumerator)
         end
     end
 
